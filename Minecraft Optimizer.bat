3c1129b7abf3245f26655f9edde0cf7ba1d8887e
BladeSkilled <109127903+bladeskilled@users.noreply.github.com>
2022-12-10 17:45:48 -0800

    Update Minecraft Optimizer.bat

--- Minecraft Optimizer.bat
+++ Minecraft Optimizer.bat
@@ -117,76 +117,76 @@ reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "MicrosoftEdg
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Microsoft Edge Update" /f >nul 2>nul
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "MicrosoftEdgeAutoLaunch_31CF12C7FD715D87B15C2DF57BBF8D3E" /f >nul 2>nul
 
-Del /S /F /Q %Windir%\Temp >nul 2>nul
+powershell Del /S /F /Q %Windir%\Temp >nul 2>nul
 
-Del /S /F /Q %windir%\Prefetch >nul 2>nul
+powershell Del /S /F /Q %windir%\Prefetch >nul 2>nul
 
-del %AppData%\vstelemetry >nul 2>nul
-del %LocalAppData%\Microsoft\VSApplicationInsights /F /Q /S >nul 2>nul
-del %ProgramData%\Microsoft\VSApplicationInsights  /F /Q /S >nul 2>nul
-del %Temp%\Microsoft\VSApplicationInsights  /F /Q /S >nul 2>nul
-del %Temp%\VSFaultInfo  /F /Q /S >nul 2>nul
-del %Temp%\VSFeedbackPerfWatsonData  /F /Q /S >nul 2>nul
-del %Temp%\VSFeedbackVSRTCLogs  /F /Q /S >nul 2>nul
-del %Temp%\VSRemoteControl  /F /Q /S >nul 2>nul
-del %Temp%\VSTelem /F /Q /S >nul 2>nul
-del %Temp%\VSTelem.Out /F /Q /S >nul 2>nul
+powershell del %AppData%\vstelemetry >nul 2>nul
+powershell del %LocalAppData%\Microsoft\VSApplicationInsights /F /Q /S >nul 2>nul
+powershell del %ProgramData%\Microsoft\VSApplicationInsights  /F /Q /S >nul 2>nul
+powershell del %Temp%\Microsoft\VSApplicationInsights  /F /Q /S >nul 2>nul
+powershell del %Temp%\VSFaultInfo  /F /Q /S >nul 2>nul
+powershell del %Temp%\VSFeedbackPerfWatsonData  /F /Q /S >nul 2>nul
+powershell del %Temp%\VSFeedbackVSRTCLogs  /F /Q /S >nul 2>nul
+powershell del %Temp%\VSRemoteControl  /F /Q /S >nul 2>nul
+powershell del %Temp%\VSTelem /F /Q /S >nul 2>nul
+powershell del %Temp%\VSTelem.Out /F /Q /S >nul 2>nul
 
-del %localappdata%\Yarn\Cache /F /Q /S >nul 2>nul
+powershell del %localappdata%\Yarn\Cache /F /Q /S >nul 2>nul
 
-del %appdata%\Microsoft\Teams\Cache /F /Q /S >nul 2>nul
+powershell del %appdata%\Microsoft\Teams\Cache /F /Q /S >nul 2>nul
 
-del %programdata%\GOG.com\Galaxy\webcache /F /Q /S >nul 2>nul
-del %programdata%\GOG.com\Galaxy\logs /F /Q /S >nul 2>nul
+powershell del %programdata%\GOG.com\Galaxy\webcache /F /Q /S >nul 2>nul
+powershell del %programdata%\GOG.com\Galaxy\logs /F /Q /S >nul 2>nul
 
-del %localappdata%\Microsoft\Windows\WebCache /F /Q /S >nul 2>nul
+powershell del %localappdata%\Microsoft\Windows\WebCache /F /Q /S >nul 2>nul
 
-del "%SystemDrive%\*.log" /F /Q >nul 2>nul
-del "%WinDir%\Directx.log" /F /Q >nul 2>nul
-del "%WinDir%\SchedLgU.txt" /F /Q >nul 2>nul
-del "%WinDir%\*.log" /F /Q >nul 2>nul
-del "%WinDir%\security\logs\*.old" /F /Q >nul 2>nul
-del "%WinDir%\security\logs\*.log" /F /Q >nul 2>nul
-del "%WinDir%\Debug\*.log" /F /Q >nul 2>nul
-del "%WinDir%\Debug\UserMode\*.bak" /F /Q >nul 2>nul
-del "%WinDir%\Debug\UserMode\*.log" /F /Q >nul 2>nul
-del "%WinDir%\*.bak" /F /Q >nul 2>nul
-del "%WinDir%\system32\wbem\Logs\*.log" /F /Q >nul 2>nul
-del "%WinDir%\OEWABLog.txt" /F /Q >nul 2>nul
-del "%WinDir%\setuplog.txt" /F /Q >nul 2>nul
-del "%WinDir%\Logs\DISM\*.log" /F /Q >nul 2>nul
-del "%WinDir%\*.log.txt" /F /Q >nul 2>nul
-del "%WinDir%\APPLOG\*.*" /F /Q >nul 2>nul
-del "%WinDir%\system32\wbem\Logs\*.log" /F /Q >nul 2>nul
-del "%WinDir%\system32\wbem\Logs\*.lo_" /F /Q >nul 2>nul
-del "%WinDir%\Logs\DPX\*.log" /F /Q >nul 2>nul
-del "%WinDir%\ServiceProfiles\NetworkService\AppData\Local\Temp\*.log" /F /Q >nul 2>nul
-del "%WinDir%\Logs\*.log" /F /Q >nul 2>nul
-del "%LocalAppData%\Microsoft\Windows\WindowsUpdate.log" /F /Q >nul 2>nul
-del "%LocalAppData%\Microsoft\Windows\WebCache\*.log" /F /Q >nul 2>nul
-del "%WinDir%\Panther\cbs.log" /F /Q >nul 2>nul
-del "%WinDir%\Panther\DDACLSys.log" /F /Q >nul 2>nul
-del "%WinDir%\repair\setup.log" /F /Q >nul 2>nul
-del "%WinDir%\Panther\UnattendGC\diagerr.xml" /F /Q >nul 2>nul
-del "%WinDir%\Panther\UnattendGC\diagwrn.xml" /F /Q >nul 2>nul
-del "%WinDir%\inf\setupapi.offline.log" /F /Q >nul 2>nul
-del "%WinDir%\inf\setupapi.app.log" /F /Q >nul 2>nul
-del "%WinDir%\debug\WIA\*.log" /F /Q >nul 2>nul
-del "%SystemDrive%\PerfLogs\System\Diagnostics\*.*" /F /Q >nul 2>nul
-del "%WinDir%\Logs\CBS\*.cab" /F /Q >nul 2>nul
-del "%WinDir%\Logs\CBS\*.cab" /F /Q >nul 2>nul
-del "%WinDir%\Logs\WindowsBackup\*.etl" /F /Q >nul 2>nul
-del "%WinDir%\System32\LogFiles\HTTPERR\*.*" /F /Q >nul 2>nul
-del "%WinDir%\SysNative\SleepStudy\*.etl" /F /Q >nul 2>nul
-del "%WinDir%\SysNative\SleepStudy\ScreenOn\*.etl" /F /Q >nul 2>nul
-del "%WinDir%\System32\SleepStudy\*.etl" /F /Q >nul 2>nul
-del "%WinDir%\System32\SleepStudy\ScreenOn\*.etl" /F /Q >nul 2>nul
-del "%WinDir%\Logs" /F /Q >nul 2>nul
-del "%WinDir%\DISM" /F /Q >nul 2>nul
-del "%WinDir%\System32\catroot2\*.chk" /F /Q >nul 2>nul
-del "%WinDir%\System32\catroot2\*.log" /F /Q >nul 2>nul
-del "%WinDir%\System32\catroot2\.jrs" /F /Q >nul 2>nul
-del "%WinDir%\System32\catroot2\*.txt" /F /Q >nul 2>nul
+powershell del "%SystemDrive%\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\Directx.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\SchedLgU.txt" /F /Q >nul 2>nul
+powershell del "%WinDir%\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\security\logs\*.old" /F /Q >nul 2>nul
+powershell del "%WinDir%\security\logs\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\Debug\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\Debug\UserMode\*.bak" /F /Q >nul 2>nul
+powershell del "%WinDir%\Debug\UserMode\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\*.bak" /F /Q >nul 2>nul
+powershell del "%WinDir%\system32\wbem\Logs\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\OEWABLog.txt" /F /Q >nul 2>nul
+powershell del "%WinDir%\setuplog.txt" /F /Q >nul 2>nul
+powershell del "%WinDir%\Logs\DISM\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\*.log.txt" /F /Q >nul 2>nul
+powershell del "%WinDir%\APPLOG\*.*" /F /Q >nul 2>nul
+powershell del "%WinDir%\system32\wbem\Logs\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\system32\wbem\Logs\*.lo_" /F /Q >nul 2>nul
+powershell del "%WinDir%\Logs\DPX\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\ServiceProfiles\NetworkService\AppData\Local\Temp\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\Logs\*.log" /F /Q >nul 2>nul
+powershell del "%LocalAppData%\Microsoft\Windows\WindowsUpdate.log" /F /Q >nul 2>nul
+powershell del "%LocalAppData%\Microsoft\Windows\WebCache\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\Panther\cbs.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\Panther\DDACLSys.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\repair\setup.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\Panther\UnattendGC\diagerr.xml" /F /Q >nul 2>nul
+powershell del "%WinDir%\Panther\UnattendGC\diagwrn.xml" /F /Q >nul 2>nul
+powershell del "%WinDir%\inf\setupapi.offline.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\inf\setupapi.app.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\debug\WIA\*.log" /F /Q >nul 2>nul
+powershell del "%SystemDrive%\PerfLogs\System\Diagnostics\*.*" /F /Q >nul 2>nul
+powershell del "%WinDir%\Logs\CBS\*.cab" /F /Q >nul 2>nul
+powershell del "%WinDir%\Logs\CBS\*.cab" /F /Q >nul 2>nul
+powershell del "%WinDir%\Logs\WindowsBackup\*.etl" /F /Q >nul 2>nul
+powershell del "%WinDir%\System32\LogFiles\HTTPERR\*.*" /F /Q >nul 2>nul
+powershell del "%WinDir%\SysNative\SleepStudy\*.etl" /F /Q >nul 2>nul
+powershell del "%WinDir%\SysNative\SleepStudy\ScreenOn\*.etl" /F /Q >nul 2>nul
+powershell del "%WinDir%\System32\SleepStudy\*.etl" /F /Q >nul 2>nul
+powershell del "%WinDir%\System32\SleepStudy\ScreenOn\*.etl" /F /Q >nul 2>nul
+powershell del "%WinDir%\Logs" /F /Q >nul 2>nul
+powershell del "%WinDir%\DISM" /F /Q >nul 2>nul
+powershell del "%WinDir%\System32\catroot2\*.chk" /F /Q >nul 2>nul
+powershell del "%WinDir%\System32\catroot2\*.log" /F /Q >nul 2>nul
+powershell del "%WinDir%\System32\catroot2\.jrs" /F /Q >nul 2>nul
+powershell del "%WinDir%\System32\catroot2\*.txt" /F /Q >nul 2>nul
 
 title Finished!
 
