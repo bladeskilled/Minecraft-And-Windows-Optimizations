e6850c8f3b85ed140058320acd64d6f3ed6204ea
BladeSkilled <109127903+bladeskilled@users.noreply.github.com>
2022-12-29 19:35:49 -0800

    Update Optimizer.bat

--- Optimizer.bat
+++ Optimizer.bat
@@ -60,57 +60,7 @@ start /b net start STR >nul 2>&1
 	if !VAR! lss 19042 (
 		bcdedit /set useplatformtick yes >nul 2>&1
 	)
+
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
 
-reg delete "HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components" /v "Microsoft Edge" /f >nul 2>nul
-reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "MicrosoftEdgeAutoLaunch_E9C49D8E9BDC4095F482C844743B9E82" /f >nul 2>nul
-reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "MicrosoftEdgeAutoLaunch" /f >nul 2>nul
-reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Microsoft Edge Update" /f >nul 2>nul
-reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "MicrosoftEdgeAutoLaunch_31CF12C7FD715D87B15C2DF57BBF8D3E" /f >nul 2>nul
-
-powershell del "%SystemDrive%\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\Directx.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\SchedLgU.txt" /F /Q >nul 2>nul
-powershell del "%WinDir%\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\security\logs\*.old" /F /Q >nul 2>nul
-powershell del "%WinDir%\security\logs\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\Debug\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\Debug\UserMode\*.bak" /F /Q >nul 2>nul
-powershell del "%WinDir%\Debug\UserMode\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\*.bak" /F /Q >nul 2>nul
-powershell del "%WinDir%\system32\wbem\Logs\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\OEWABLog.txt" /F /Q >nul 2>nul
-powershell del "%WinDir%\setuplog.txt" /F /Q >nul 2>nul
-powershell del "%WinDir%\Logs\DISM\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\*.log.txt" /F /Q >nul 2>nul
-powershell del "%WinDir%\APPLOG\*.*" /F /Q >nul 2>nul
-powershell del "%WinDir%\system32\wbem\Logs\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\system32\wbem\Logs\*.lo_" /F /Q >nul 2>nul
-powershell del "%WinDir%\Logs\DPX\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\ServiceProfiles\NetworkService\AppData\Local\Temp\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\Logs\*.log" /F /Q >nul 2>nul
-powershell del "%LocalAppData%\Microsoft\Windows\WindowsUpdate.log" /F /Q >nul 2>nul
-powershell del "%LocalAppData%\Microsoft\Windows\WebCache\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\Panther\cbs.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\Panther\DDACLSys.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\repair\setup.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\Panther\UnattendGC\diagerr.xml" /F /Q >nul 2>nul
-powershell del "%WinDir%\Panther\UnattendGC\diagwrn.xml" /F /Q >nul 2>nul
-powershell del "%WinDir%\inf\setupapi.offline.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\inf\setupapi.app.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\debug\WIA\*.log" /F /Q >nul 2>nul
-powershell del "%SystemDrive%\PerfLogs\System\Diagnostics\*.*" /F /Q >nul 2>nul
-powershell del "%WinDir%\Logs\CBS\*.cab" /F /Q >nul 2>nul
-powershell del "%WinDir%\Logs\CBS\*.cab" /F /Q >nul 2>nul
-powershell del "%WinDir%\Logs\WindowsBackup\*.etl" /F /Q >nul 2>nul
-powershell del "%WinDir%\System32\LogFiles\HTTPERR\*.*" /F /Q >nul 2>nul
-powershell del "%WinDir%\SysNative\SleepStudy\*.etl" /F /Q >nul 2>nul
-powershell del "%WinDir%\SysNative\SleepStudy\ScreenOn\*.etl" /F /Q >nul 2>nul
-powershell del "%WinDir%\System32\SleepStudy\*.etl" /F /Q >nul 2>nul
-powershell del "%WinDir%\System32\SleepStudy\ScreenOn\*.etl" /F /Q >nul 2>nul
-powershell del "%WinDir%\Logs" /F /Q >nul 2>nul
-powershell del "%WinDir%\DISM" /F /Q >nul 2>nul
-powershell del "%WinDir%\System32\catroot2\*.chk" /F /Q >nul 2>nul
-powershell del "%WinDir%\System32\catroot2\*.log" /F /Q >nul 2>nul
-powershell del "%WinDir%\System32\catroot2\.jrs" /F /Q >nul 2>nul
-powershell del "%WinDir%\System32\catroot2\*.txt" /F /Q >nul 2>nul
+powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
